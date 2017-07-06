cf0b43b4f3af1302ce86a85ca551087ae4f6006d
limao <limao@wireless-10-104-117-143.umd.edu>
2017-07-06 13:46:53 -0400

    Initial Commit

---
+++ Locus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locus
+//
+//  Created by limao on 7/6/17.
+//  Copyright © 2017 limao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
